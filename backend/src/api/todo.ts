b4cc6145259f8a0d83d1b3e68ab1ac48a39b80c1
xnacly <47723417+xNaCly@users.noreply.github.com>
2022-10-31 09:16:53 +0100

    feat(api): dockerfile and test CRUD

--- backend/src/api/todo.ts
+++ backend/src/api/todo.ts
@@ -5,4 +5,16 @@ router.get("/", (req: Request, res: Response) => {
 	res.send({ hello: "world" });
 });
 
+router.post("/", (req: Request, res: Response) => {
+	res.status(201).send({ hello: "world" });
+});
+
+router.delete("/:id", (req: Request, res: Response) => {
+	res.sendStatus(200);
+});
+
+router.put("/:id", (req: Request, res: Response) => {
+	res.sendStatus(200);
+});
+
 export default router;
