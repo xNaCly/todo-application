e3adb394f29b3309ab6834ab8dfb23466465b31d
xnacly <47723417+xNaCly@users.noreply.github.com>
2022-10-31 09:16:53 +0100

    feat(api): dockerfile and test CRUD

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,12 +1,14 @@
 import express from "express";
 import { log, LogType } from "./log";
-const app = express();
-
-const PORT = 8080;
-
 import todo from "./api/todo";
+import bodyParser from "body-parser";
 
-app.listen(PORT, () => {
+const app = express();
+const PORT = 8080;
+const HOST = "0.0.0.0";
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.listen(PORT, HOST, () => {
 	log(LogType.INFO, `server listening on ${PORT}`);
 });
 
