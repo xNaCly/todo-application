e724375d8b2141c1e2b8ec0e2ae310db74e48662
xnacly <47723417+xNaCly@users.noreply.github.com>
2022-10-31 07:47:56 +0100

    fix: fix issues using typehinting

--- backend/src/api/todo.ts
+++ backend/src/api/todo.ts
@@ -1,7 +1,7 @@
-import express from "express";
+import express, { Request, Response } from "express";
 const router = express.Router();
 
-router.get("/", (req, res) => {
+router.get("/", (req: Request, res: Response) => {
 	res.send({ hello: "world" });
 });
 
