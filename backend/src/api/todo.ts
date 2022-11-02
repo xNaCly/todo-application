869bf818f5375edd36483d875e92a41026c8b98b
xnacly <47723417+xNaCly@users.noreply.github.com>
2022-11-02 11:35:40 +0100

    feat(api): all /todo HTTP methods
    - GET by id
    - GET all
    - POST
    - PATCH
    - DELETE

--- backend/src/api/todo.ts
+++ backend/src/api/todo.ts
@@ -1,20 +1,81 @@
 import express, { Request, Response } from "express";
+import db from "../database";
+import { errorFeedback } from "../util";
 const router = express.Router();
 
-router.get("/", (req: Request, res: Response) => {
-	res.send({ hello: "world" });
+router.get("/", async (req: Request, res: Response) => {
+	const todos: Todo[] = (await db.getTasks()) || [];
+
+	res.send({ data: todos });
 });
 
-router.post("/", (req: Request, res: Response) => {
-	res.status(201).send({ hello: "world" });
+router.get("/:id", async (req: Request, res: Response) => {
+	let { id } = req.params;
+	let convertedId = parseInt(id);
+
+	if (Number.isNaN(convertedId)) {
+		let err = errorFeedback(400, "missing request parameter id");
+		return res.status(err.code).send({ err });
+	}
+
+	const todo = await db.getTaskById(parseInt(id));
+
+	if (!todo) {
+		let err = errorFeedback(404, `todo with id '${convertedId}' not found`);
+		return res.status(err.code).send({ err });
+	}
+
+	res.send({ data: todo });
 });
 
-router.delete("/:id", (req: Request, res: Response) => {
+router.post("/", async (req: Request, res: Response) => {
+	const { title }: { title: string } = req.body;
+
+	if (!title || title.length === 0) {
+		let err = errorFeedback(400, "missing or empty request body key value pair: 'title'");
+		return res.status(err.code).send({ err });
+	}
+
+	let todo = await db.insertNewTask(title);
+	res.status(201).send({ data: todo });
+});
+
+router.delete("/:id", async (req: Request, res: Response) => {
+	let { id } = req.params;
+	let convertedId = parseInt(id);
+
+	if (Number.isNaN(convertedId)) {
+		let err = errorFeedback(400, "missing request parameter id");
+		return res.status(err.code).send({ err });
+	}
+
+	const todo = await db.deleteTaskById(convertedId);
+
+	if (!todo) {
+		let err = errorFeedback(500, `deleting todo with id '${id}' failed`);
+		return res.status(err.code).send({ err });
+	}
+
 	res.sendStatus(200);
 });
 
-router.put("/:id", (req: Request, res: Response) => {
-	res.sendStatus(200);
+router.patch("/:id", async (req: Request, res: Response) => {
+	const { title }: { title: string } = req.body;
+	let { id } = req.params;
+	let convertedId = parseInt(id);
+
+	if (!title || title.length === 0) {
+		let err = errorFeedback(400, "missing or empty request body key value pair: 'title'");
+		return res.status(err.code).send({ err });
+	}
+
+	if (Number.isNaN(convertedId)) {
+		let err = errorFeedback(400, "missing request parameter id");
+		return res.status(err.code).send({ err });
+	}
+
+	const todo = await db.updateTaskTitleById(convertedId, title);
+	res.status(200).send({ data: todo });
 });
 
 export default router;
