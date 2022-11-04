9fa76b579ea345cca050c714dbad48f7d725591b
xnacly <47723417+xNaCly@users.noreply.github.com>
2022-11-04 12:44:04 +0100

    feat(web): implement Task managment logic
    - adding
    - removing
    - finishing
    tasks
    
    each of the above triggers a REST request using the api wrapper custom fetch method

--- frontend/src/components/NewTaskButton.js
+++ frontend/src/components/NewTaskButton.js
@@ -1,24 +1,31 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 
-export default function NewTaskButton() {
+export default function NewTaskButton({ addANewTaskByTitle }) {
 	const [toggled, setToggle] = useState(false);
-	const [submit, setSubmit] = useState(false);
+	const [taskTitle, setTasktitle] = useState("");
+
+	function submitTask() {
+		addANewTaskByTitle(taskTitle);
+		setToggle(false);
+		setTasktitle("");
+	}
 
 	const createModal = (
 		<div className="task-modal">
-			<button onClick={() => setToggle(!toggled)}>{"[X]"}</button>
-			<div>
-				<h5>New Todo title:</h5>
-				<input type="text"></input>
-				<button>{"[submit]"}</button>
-			</div>
+			<h5>New Todo title:</h5>
+			<input onChange={(e) => setTasktitle(e.target.value)} type="text"></input>
+			<button title="create new todo" onClick={() => submitTask()}>
+				{"[↵ ]"}
+			</button>
 		</div>
 	);
 
 	return (
 		<div>
 			{toggled ? createModal : <div></div>}
-			<button onClick={() => setToggle(!toggled)}>{`[${toggled ? "-" : "+"}]`}</button>
+			<button title={toggled ? "close" : "new"} onClick={() => setToggle(!toggled)}>{`[${
+				toggled ? "close" : "new"
+			}]`}</button>
 		</div>
 	);
 }
--- frontend/src/components/Task.js
+++ frontend/src/components/Task.js
@@ -1,13 +1,20 @@
-export default function Task({ title, createdAt, finished, id }) {
+export default function Task({ taskRemoveMethod, taskUpdaterMethod, title, createdAt, finished, id }) {
 	let date = new Date(createdAt);
 	return (
 		<div className={`task ${finished ? "task-finished" : ""}`}>
-			<span className="task-counter">{id}</span>
+			<div className="task-controller">
+				<span className="task-counter">{id}</span>
+				<button
+					title={finished ? "mark task as open" : "mark task as finished"}
+					onClick={() => taskUpdaterMethod({ title, createdAt, id, finished: !finished })}>{`[${
+					finished ? "open" : "done"
+				}]`}</button>
+				<button title="delete task" onClick={() => taskRemoveMethod(id)}>
+					{"[delete]"}
+				</button>
+			</div>
 			<div className="task-content">
-				<div>
-					<button onClick={() => console.log("test")}>{`[${finished ? "#" : " "}]`}</button>
-					<span className="task-title">{title}</span>
-				</div>
+				<span className="task-title">{title}</span>
 				<span className="task-date">{date.toLocaleString()}</span>
 			</div>
 		</div>
