4ca9643e3b7ada4485243c8d247a4c0ad9d8df0e
xnacly <47723417+xNaCly@users.noreply.github.com>
2022-11-04 12:44:04 +0100

    feat(web): implement Task managment logic
    - adding
    - removing
    - finishing
    tasks
    
    each of the above triggers a REST request using the api wrapper custom fetch method

--- frontend/src/components/Tasklist.js
+++ frontend/src/components/Tasklist.js
@@ -1,11 +1,59 @@
+import { useEffect, useState } from "react";
 import NewTaskButton from "./NewTaskButton";
+import { getAllTasks, addTask, updateTask, removeTask } from "../api-wrapper";
 import Task from "./Task";
-export default function TaskList({ tasks }) {
-	let renderedTasks = tasks.map((e) => <Task {...e} key={e?.id} />);
-	return (
-		<div className="tasklist">
-			{renderedTasks}
-			<NewTaskButton />
-		</div>
-	);
+import errormodal from "./errormodal";
+
+export default function TaskList() {
+	const [tasks, setTasks] = useState([]);
+	const [error, setError] = useState(false);
+
+	useEffect(() => {
+		const f = async () => {
+			let res = await getAllTasks();
+			setError(errormodal(setError, res));
+			if (res?.data) setTasks(res?.data);
+			else setTasks([]);
+		};
+		f();
+	}, []);
+
+	function addATask(newTask) {
+		let f = async () => {
+			let res = await updateTask(newTask);
+			setError(errormodal(setError, res));
+		};
+		f();
+		let newArr = [...tasks.filter((e) => e?.id !== newTask?.id), newTask].sort((a, b) => a?.id - b?.id);
+		setTasks(newArr);
+	}
+
+	function addANewTaskByTitle(title) {
+		let f = async () => {
+			let res = await addTask(title);
+			setError(errormodal(setError, res));
+			if (res?.data) setTasks([...tasks, res?.data]);
+			else setTasks(tasks);
+		};
+		f();
+	}
+
+	function taskRemoveMethod(id) {
+		removeTask(id);
+		setTasks([...tasks.filter((e) => e?.id !== id)]);
+	}
+
+	let renderedTasks = tasks?.map((e) => (
+		<Task taskRemoveMethod={taskRemoveMethod} taskUpdaterMethod={addATask} {...e} key={e?.id} />
+	));
+	if (!error) {
+		return (
+			<div className="tasklist">
+				{renderedTasks}
+				<NewTaskButton addANewTaskByTitle={addANewTaskByTitle} />
+			</div>
+		);
+	} else {
+		return error;
+	}
 }
