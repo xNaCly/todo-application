b6433bd4695991b71f752a2a84e4baa81f44189b
xnacly <47723417+xNaCly@users.noreply.github.com>
2022-11-04 15:11:03 +0100

    refactor: remove unused import

--- frontend/src/components/NewTaskButton.js
+++ frontend/src/components/NewTaskButton.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from "react";
+import React, { useState } from "react";
 
 export default function NewTaskButton({ addANewTaskByTitle }) {
 	const [toggled, setToggle] = useState(false);
