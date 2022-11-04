d1b822f3f6fca90b6096621df86e09c9f19e1059
xnacly <47723417+xNaCly@users.noreply.github.com>
2022-11-04 22:14:00 +0100

    fix: add cors to backend, somehow it worked before
    ill say it: fuck cors and why the fuck is it that fucking inconsistent - works on 2 machines, fails on one

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,6 +2,7 @@ import express from "express";
 import { log, LogType } from "./util";
 import todo from "./api/todo";
 import bodyParser from "body-parser";
+import cors from "cors";
 
 const app = express();
 const PORT = 8080;
@@ -9,6 +10,7 @@ const HOST = "0.0.0.0";
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(cors({origin: '*'}));
 
 app.listen(PORT, HOST, () => {
 	log(LogType.INFO, `server listening on ${PORT}`);
