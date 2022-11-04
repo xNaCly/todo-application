b052c07982600d775368f1fb3e7ad2fab5dc7884
xnacly <47723417+xNaCly@users.noreply.github.com>
2022-11-04 12:42:31 +0100

    feat(web): remove placeholders

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,37 +1,10 @@
 import "./App.css";
 import TaskList from "./components/Tasklist";
 
-const exampleTasks = [
-	{
-		title: "title",
-		createdAt: Date.now(),
-		finished: true,
-		id: 1,
-	},
-	{
-		title: "title",
-		createdAt: Date.now(),
-		finished: false,
-		id: 2,
-	},
-	{
-		title: "title",
-		createdAt: Date.now(),
-		finished: false,
-		id: 3,
-	},
-	{
-		title: "title",
-		createdAt: Date.now(),
-		finished: false,
-		id: 4,
-	},
-];
-
 function App() {
 	return (
 		<div id="App">
-			<TaskList tasks={exampleTasks}></TaskList>
+			<TaskList />
 		</div>
 	);
 }
